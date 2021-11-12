dfd312af1b40c2e6ff1b2a29aec489b8aab65a63
sl0805 <957309025@qq.com>
2021-11-12 18:07:48 +0800

    second

--- config.py
+++ config.py
@@ -1,16 +1,34 @@
-import os
-
+'''
+Author: your name
+Date: 2021-11-10 15:47:18
+LastEditTime: 2021-11-12 18:00:51
+LastEditors: Please set LastEditors
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: \python\teamwork\app\config.py
+'''
+# import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 DEBUG = True
 
-SECRET_KEY = os.urandom(24)
-
+# SECRET_KEY = os.urandom(24)
+'''
 HOSTNAME = ''
 PORT = ''
 DATABASE = ''
 USERNAME = ''
 PASSWORD = ''
+'''
+'''
 DB_URI = 'mysql+mysqldb://{}:{}@{}:{}/{}?charset=utf8'.format(
     USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+'''
+app = Flask(__name__)
+app.secret_key = "123456"
+# 绑定mysql
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:密码@127.0.0.1:3306/sio'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "jamchaos666"
--- manage.py
+++ manage.py
@@ -1,3 +1,11 @@
+'''
+Author: your name
+Date: 2021-11-10 15:47:18
+LastEditTime: 2021-11-12 18:05:33
+LastEditors: your name
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%A
+FilePath: \python\teamwork\manage.py
+'''
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from all import app
